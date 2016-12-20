3c3ba39d923613ebfba1613f6d8f2bd60cb69d89
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-12-20 15:43:55 +0100

    updated the API in accordance with the new data model/ the new data

--- src/server.js
+++ src/server.js
@@ -1,19 +1,10 @@
 var sqlite3 = require('sqlite3').verbose();
 var express = require('express');
 var cors = require('cors');
-var db = new sqlite3.Database('./data/entities.db');
+var db = new sqlite3.Database('./data/storyteller.db');
 var app = express();
 
 app.use(cors());
-
-app.get('/', function (req, res, next) {
-        db.all("SELECT * FROM nodes", function (err, rows) {
-            if(err !== null) {
-                return next(err);
-            }
-            res.status(200).send(rows);
-        });
-});
 
 app.get('/list', function (req, res, next) {
         db.all("SELECT name FROM sqlite_master WHERE type = 'table';", function (err, rows) {
@@ -24,8 +15,8 @@ app.get('/list', function (req, res, next) {
         });
 });
 
-app.get('/node/:id', function (req, res, next) {
-        db.all("SELECT * FROM nodes WHERE id = ?", req.params.id, function (err, rows) {
+app.get('/entities/:id', function (req, res, next) {
+        db.all("SELECT * FROM entities WHERE id = ?", req.params.id, function (err, rows) {
             if(err !== null) {
                 return next(err);
             } else if (rows.length === 0) {
@@ -36,8 +27,8 @@ app.get('/node/:id', function (req, res, next) {
         });
 });
 
-app.get('/node/:id/children', function (req, res, next) {
-        db.all("SELECT * FROM nodes WHERE childof = ? ORDER BY isentity DESC, name ASC", req.params.id, function (err, rows) {
+app.get('/entities/:id/children', function (req, res, next) {
+        db.all("SELECT * FROM entities WHERE childof = ? ORDER BY isinstance ASC, name ASC", req.params.id, function (err, rows) {
             if(err !== null) {
                 return next(err);
             } else if (rows.length === 0) {
@@ -48,15 +39,77 @@ app.get('/node/:id/children', function (req, res, next) {
         });
 });
 
-app.get('/root', function (req, res, next) {
-        db.all("SELECT * FROM nodes WHERE id=1", function (err, rows) {
+app.get('/events/:id', function (req, res, next) {
+        db.all("SELECT * FROM events WHERE id = ?", req.params.id, function (err, rows) {
             if(err !== null) {
                 return next(err);
+            } else if (rows.length === 0) {
+                console.log(err);
+                return res.status(404).send({ error : "ID doesn't exist" });
             }
             res.status(200).send(rows);
         });
 });
 
+app.get('/events/:id/children', function (req, res, next) {
+        db.all("SELECT * FROM events WHERE childof = ? ORDER BY isinstance ASC, name ASC", req.params.id, function (err, rows) {
+            if(err !== null) {
+                return next(err);
+            } else if (rows.length === 0) {
+                console.log(err);
+                return res.status(404).send({ error : "ID doesn't exist" });
+            }
+            res.status(200).send(rows);
+        });
+});
+
+app.get('/sources/:id', function (req, res, next) {
+        db.all("SELECT * FROM sources WHERE id = ?", req.params.id, function (err, rows) {
+            if(err !== null) {
+                return next(err);
+            } else if (rows.length === 0) {
+                console.log(err);
+                return res.status(404).send({ error : "ID doesn't exist" });
+            }
+            res.status(200).send(rows);
+        });
+});
+
+app.get('/sources/:id/children', function (req, res, next) {
+        db.all("SELECT * FROM sources WHERE childof = ? ORDER BY isinstance ASC, name ASC", req.params.id, function (err, rows) {
+            if(err !== null) {
+                return next(err);
+            } else if (rows.length === 0) {
+                console.log(err);
+                return res.status(404).send({ error : "ID doesn't exist" });
+            }
+            res.status(200).send(rows);
+        });
+});
+
+app.get('/topics/:id', function (req, res, next) {
+        db.all("SELECT * FROM topics WHERE id = ?", req.params.id, function (err, rows) {
+            if(err !== null) {
+                return next(err);
+            } else if (rows.length === 0) {
+                console.log(err);
+                return res.status(404).send({ error : "ID doesn't exist" });
+            }
+            res.status(200).send(rows);
+        });
+});
+
+app.get('/topics/:id/children', function (req, res, next) {
+        db.all("SELECT * FROM topics WHERE childof = ? ORDER BY isinstance ASC, name ASC", req.params.id, function (err, rows) {
+            if(err !== null) {
+                return next(err);
+            } else if (rows.length === 0) {
+                console.log(err);
+                return res.status(404).send({ error : "ID doesn't exist" });
+            }
+            res.status(200).send(rows);
+        });
+});
 
 app.listen(5000, function(){
     console.log('Example app listening on port 5000!');
